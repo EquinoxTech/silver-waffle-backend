93b96ab82afea55e61710ad46bce8fbe6c6f45c1
Rhian Castro <rhi.castro@gmail.com>
2021-11-16 18:10:06 -0300

    feat: auto swagger generation module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OngsModule } from './ongs/ongs.module';
 
 @Module({
-  imports: [],
+  imports: [OngsModule],
   controllers: [AppController],
   providers: [AppService],
 })
