4f638fa339468c06fd9ef661024e030a38d7ee93
Lucira Silva <luciracs0@gmail.com>
2021-11-18 21:19:27 -0300

    feat: create database config, connection and create ong entity

--- src/ongs/entities/ong.entity.ts
+++ src/ongs/entities/ong.entity.ts
@@ -1,1 +1,43 @@
-export class Ong {}
+import { Point } from 'geojson';
+import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
+
+@Entity()
+export class Ong {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar')
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('varchar')
+  working_hours: string;
+
+  @Column('varchar')
+  street: string;
+
+  @Column('int')
+  number: number;
+
+  @Column('varchar')
+  state: string;
+
+  @Column('varchar')
+  city: string;
+
+  @Column('varchar')
+  neighbourhood: string;
+
+  @Index({ spatial: true })
+  @Column({
+    type: 'point',
+    nullable: true,
+    srid: 4326,
+  })
+  location: Point;
+
+  @Column()
+  responsible: string;
+}
